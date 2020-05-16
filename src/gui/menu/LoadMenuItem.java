e0692d509c284c2a4e5daecdc684f43949a1d695
Jonatan <Jonatan@Skynet>
2020-05-16 16:23:14 +0200

    save/load implemented

--- src/gui/menu/LoadMenuItem.java
+++ src/gui/menu/LoadMenuItem.java
@@ -2,17 +2,21 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+import model.Sheet;
+
 import java.io.FileNotFoundException;
 import javax.swing.JFileChooser;
 
 class LoadMenuItem extends OpenMenuItem {
- 
-    public LoadMenuItem(XL xl, StatusLabel statusLabel) {
+	Sheet sheet;
+    public LoadMenuItem(XL xl, StatusLabel statusLabel, Sheet sheet) {
         super(xl, statusLabel, "Load");
+        this.sheet = sheet;
     }
 
     protected void action(String path) throws FileNotFoundException {
-        // TODO
+       sheet.clear();
+       sheet.load(path);
     }
 
     protected int openDialog(JFileChooser fileChooser) {
