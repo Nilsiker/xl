4704f6d5eb7b84901e4c0442be4896ce02296b00
Jonatan <Jonatan@Skynet>
2020-05-16 15:18:26 +0200

    implement clear

--- src/gui/menu/ClearMenuItem.java
+++ src/gui/menu/ClearMenuItem.java
@@ -4,13 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JMenuItem;
 
+import gui.CurrentSlot;
+import model.Sheet;
+
 class ClearMenuItem extends JMenuItem implements ActionListener {
-    public ClearMenuItem() {
+	Sheet sheet;
+	CurrentSlot currentSlot;
+    public ClearMenuItem(Sheet sheet, CurrentSlot currentSlot) {
         super("Clear");
         addActionListener(this);
+        this.sheet = sheet;
+        this.currentSlot = currentSlot;
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO
+        sheet.removeSlot(currentSlot.getAddress());
     }
 }
--- src/gui/menu/XLMenuBar.java
+++ src/gui/menu/XLMenuBar.java
@@ -1,5 +1,6 @@
 package gui.menu;
 
+import gui.CurrentSlot;
 import gui.Editor;
 import gui.StatusLabel;
 import gui.XL;
@@ -10,7 +11,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
 public class XLMenuBar extends JMenuBar {
-    public XLMenuBar(XL xl, XLList xlList, StatusLabel statusLabel, Sheet sheet) {
+    public XLMenuBar(XL xl, XLList xlList, StatusLabel statusLabel, Sheet sheet, CurrentSlot currentSlot) {
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
         file.add(new PrintMenuItem(xl, statusLabel));
@@ -18,7 +19,7 @@ public class XLMenuBar extends JMenuBar {
         file.add(new LoadMenuItem(xl, statusLabel));
         file.add(new NewMenuItem(xl));
         file.add(new CloseMenuItem(xl, xlList));
-        edit.add(new ClearMenuItem());
+        edit.add(new ClearMenuItem(sheet, currentSlot));
         edit.add(new ClearAllMenuItem(sheet));
         add(file);
         add(edit);
